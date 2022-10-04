6aa44839e410fae74fcfb270d050cd989bb9f139
thebaolin <bchang916@gmail.com>
2022-10-03 20:52:50 -0400

    updated testing stuffs

--- tests.cpp
+++ tests.cpp
@@ -3,3 +3,15 @@
 #include "funcs.h"
 
 // add your tests here
+
+TEST_CASE("Testing isDivisibleBy"){
+    CHECK(isDivisibleBy(100,25) == true);
+    //CHECK(isDivisibleBy(0,0) == true); edge cases
+    //CHECK(isDivisibleBy(1,0) == false); edge cases
+    CHECK(isDivisibleBy(1,1) == true);
+
+}
+
+TEST_CASE("Testing isPrime"){
+    CHECK(isPrime(0) == false);
+}
