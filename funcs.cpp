56f735a0d042307c65dedb485f462262f2e66358
thebaolin <bchang916@gmail.com>
2022-10-03 20:32:16 -0400

    updated lab 5 files

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,73 @@
 #include <iostream>
 #include "funcs.h"
 
-// add functions here
+bool isDivisibleBy(int n, int d){
+  if(n % d == 0)
+    return true;
+  else
+    return false;
+}
+
+bool isPrime(int n){
+  for(int i = 2; i < n-1; i++){
+    if(n % i == 0)
+      return false;
+  }
+  return true;
+}
+
+int nextPrime(int n){
+  int num = n;
+    bool programfinished = false;
+    while(!isPrime(num)){
+      num++;
+      if(isPrime(n)){
+        programfinished = true;
+        }
+    }
+  return num;
+}
+
+int countPrimes(int a, int b){
+  int count = 0;
+  for(int i = a; i <=b; i++){
+    if(isPrime(i))
+      count++;
+  }
+  return count;
+}
+
+bool isTwinPrime(int n){
+  if(isPrime(n)){
+    if(isPrime(n-2) || isPrime(n+2))
+      return true;
+  }
+  return false;
+}
+
+int nextTwinPrime(int n){
+  int num = n+1;
+    bool programfinished = false;
+    while(!isTwinPrime(num)){
+      num++;
+      if(isTwinPrime(n)){
+        programfinished = true;
+        }
+    }
+  return num;
+}
+
+int largestTwinPrime(int a, int b){
+  int max = 0;
+  for(int i = a; i <=b; i++){
+    if(isTwinPrime(i)){
+      if(max < i)
+        max = i;
+    }
+  }
+  if(max == 0)
+    return -1;
+  else
+    return max;
+}
 
--- main.cpp
+++ main.cpp
@@ -2,8 +2,23 @@
 
 
 #include "funcs.h"
+using namespace std; //remember to get rid of this later
 
 int main()
 {
+  if(isDivisibleBy(100,25))
+    cout<<"Yes"<<endl;
+  else
+    cout<<"No"<<endl;
+
+  if(isPrime(7));
+    cout<<"Yes"<<endl;
+
+  cout<<nextPrime(14)<<endl;
+  cout<<countPrimes(2,7)<<endl;
+  cout<<isTwinPrime(3)<<endl;
+  cout<<nextTwinPrime(7)<<endl;
+  cout<<largestTwinPrime(14, 16)<<endl;
+
   return 0;
 }
